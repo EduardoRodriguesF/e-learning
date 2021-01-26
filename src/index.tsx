1b924d06164f477bad203b056ac5d9a4af0285e9
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-01-26 14:04:21 -0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>e.learning</Text>
+    </View>
+  )
+}
+
+export default App;

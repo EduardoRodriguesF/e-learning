bcfaf8822e91acb6df568f580e6fe88673c35ff0
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-01-27 13:48:28 -0300

    feat: lessons list screen

--- src/pages/Course/index.tsx
+++ src/pages/Course/index.tsx
@@ -1,11 +1,91 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Text, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import api from '../../services/api';
+
+import Content from '../../components/Content';
+
+import { 
+  Container,
+  Header,
+  ContentHeader,
+  Title,
+  LessonsText,
+  LessonsContainer,
+  LessonCard,
+  PlayButton,
+  LessonInfo,
+  LessonTitle,
+  LessonDetails,
+  LessonDetailText,
+  Clock,
+} from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+interface Params {
+  id: number;
+}
+interface Course {
+  id: number;
+  title: string;
+  lessons: {
+    id: number;
+    title: string; 
+  }[];
+}
 
 const Course: React.FC = () => {
+  const [course, setCourse] = useState({} as Course);
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    async function loadCourse() {
+      const { data } = await api.get(`/courses/${routeParams.id}`);
+
+      setCourse(data);
+    }
+
+    loadCourse();
+  }, [routeParams]);
+
   return (
-    <View>
-      <Text>Course</Text>
-    </View>
+    <Container>
+      <Header>
+        <Icon onPress={() => navigation.goBack()} name="arrow-left" size={24} color="#FF6680" />
+        <Image source={logoImg} />
+        <Icon name="heart" size={24} color="#FF6680" />
+      </Header>
+      <Content>
+        <ContentHeader>
+          <Title>{course.title}</Title>
+          <LessonsText>{course.lessons && course.lessons.length} aulas</LessonsText>
+        </ContentHeader>
+        <LessonsContainer>
+          {course.lessons.map(l => (
+            <LessonCard key={l.id}>
+              <PlayButton>
+                <Icon name="play-circle" size={40} color="#fff" />
+              </PlayButton>
+              <LessonInfo>
+                <LessonTitle>{l.title}</LessonTitle>
+                <LessonDetails>
+                  <LessonDetailText>Aula 01</LessonDetailText>
+                  <Clock name="clock" size={12} color="#C4C4D1" />
+                  <LessonDetailText>5 min</LessonDetailText>
+                </LessonDetails>
+              </LessonInfo>
+            </LessonCard>
+          ))}
+        </LessonsContainer>
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/pages/Course/styles.ts
@@ -0,0 +1,90 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Feather';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  padding: 52px 21px 32px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ContentHeader = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+export const Title = styled.Text`
+  color: #3D3D4C;
+  font-size: 30px;
+`;
+
+export const LessonsText = styled.Text` 
+  color: #A0A0B2;
+  font-size: 15px;
+`;
+
+export const LessonsContainer = styled.View`
+  width: 100%;
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const LessonCard = styled.View`
+  width: 100%;
+  flex-direction: row;
+  height: 100px;
+  margin-bottom: 16px;
+`;
+
+export const PlayButton = styled(RectButton)`
+  position: relative;
+  z-index: 1;
+  margin-top: 16px;
+  width: 68px;
+  height: 68px;
+  margin-right: -32px;
+  border-radius: 16px;
+  background-color: #61C5BD;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LessonInfo = styled(RectButton)`
+  width: 90%;
+  padding: 16px;
+  padding-left: 56px;
+  justify-content: space-between;
+  background-color: #fff;
+  border-radius: 16px;
+`;
+
+export const LessonTitle = styled.Text`
+  color: #6C6C80;
+  font-size: 15px;
+  width: 140px;
+`;
+
+export const LessonDetails = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const LessonDetailText = styled.Text`
+  color: #C4C4D1;
+  font-size: 10px;
+  margin-right: 16px;
+`;
+
+export const Clock = styled(Icon)`
+  margin-right: 4px;
+`;
